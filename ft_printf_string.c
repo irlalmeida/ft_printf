f8d2409a71dceca6e6c7d3547a82109cccb12e28
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-06-03 12:26:27 -0300

    back-to-simple

--- ft_printf_string.c
+++ ft_printf_string.c
@@ -6,23 +6,18 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 16:51:49 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/06/01 10:38:15 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/06/03 12:25:40 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			ft_putchar_fd(char c, int fd);
-
 int	ft_printf_string(const char *str)
 {
-	int	i;
-
-	i = 0;
 	while (*str != '\0')
 	{
-		ft_putchar_fd(str[i], 1);
-		i++;
+		write(1, str, 1);
+		str++;
 	}
 	return (0);
-}
+}	
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "ft_printf.h"
- 
 int	main(void)
 {
 	ft_printf("Hello World!\n");
-	ft_printf("Hello %d!\n", 42);
+	/*ft_printf("Hello %d!\n", 42);*/
 }
